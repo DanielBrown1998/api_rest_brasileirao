f72812a10d1ceb5103b9d6bc531ba0f242d3665f
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-09-21 11:17:49 -0300

    test: replace test widget

--- lib/ui/home/logic/home_cubic.dart
+++ lib/ui/home/logic/home_cubic.dart
@@ -10,44 +10,44 @@ class HomeCubic extends Cubit<HomeState> {
   List<Championship>? championschips;
 
   //for test only
-  Map<String, dynamic> brasileiraoB = {
-    "campeonato_id": 14,
-    "nome": "Campeonato Brasileiro Série B",
-    "slug": "campeonato-brasileiro-serie-b",
-    "nome_popular": "Brasileirão Série B",
-    "edicao_atual": {
-      "edicao_id": 180,
-      "temporada": "2025",
-      "nome": "Campeonato Brasileiro Série B 2025",
-      "nome_popular": "Brasileirão Série B 2025",
-      "slug": "campeonato-brasileiro-serie-b-2025",
-    },
-    "fase_atual": {
-      "fase_id": 769,
-      "nome": "Fase única",
-      "slug": "brasileiro-serie-b-2025-fase-unica",
-      "tipo": "pontos-corridos",
-      "_link": "/v1/campeonatos/14/fases/769",
-    },
-    "rodada_atual": {
-      "nome": "27ª Rodada",
-      "slug": "27a-rodada",
-      "rodada": 27,
-      "status": "agendada",
-    },
-    "status": "andamento",
-    "tipo": "Pontos Corridos",
-    "logo":
-        "https://api.api-futebol.com.br/images/competicao/brasileiro-serieb.png",
-    "regiao": "nacional",
-    "_link": "/v1/campeonatos/14",
-    "fases": "",
-  };
-  Future<List<Championship>> getCampeonato() async {
-    Future.delayed(Duration(seconds: 2));
-    final Championship campeonatoSerieB = Championship.fromMap(brasileiraoB);
-    return [campeonatoSerieB];
-  }
+  // Map<String, dynamic> brasileiraoB = {
+  //   "campeonato_id": 14,
+  //   "nome": "Campeonato Brasileiro Série B",
+  //   "slug": "campeonato-brasileiro-serie-b",
+  //   "nome_popular": "Brasileirão Série B",
+  //   "edicao_atual": {
+  //     "edicao_id": 180,
+  //     "temporada": "2025",
+  //     "nome": "Campeonato Brasileiro Série B 2025",
+  //     "nome_popular": "Brasileirão Série B 2025",
+  //     "slug": "campeonato-brasileiro-serie-b-2025",
+  //   },
+  //   "fase_atual": {
+  //     "fase_id": 769,
+  //     "nome": "Fase única",
+  //     "slug": "brasileiro-serie-b-2025-fase-unica",
+  //     "tipo": "pontos-corridos",
+  //     "_link": "/v1/campeonatos/14/fases/769",
+  //   },
+  //   "rodada_atual": {
+  //     "nome": "27ª Rodada",
+  //     "slug": "27a-rodada",
+  //     "rodada": 27,
+  //     "status": "agendada",
+  //   },
+  //   "status": "andamento",
+  //   "tipo": "Pontos Corridos",
+  //   "logo":
+  //       "https://api.api-futebol.com.br/images/competicao/brasileiro-serieb.png",
+  //   "regiao": "nacional",
+  //   "_link": "/v1/campeonatos/14",
+  //   "fases": "",
+  // };
+  // Future<List<Championship>> getCampeonato() async {
+  //   Future.delayed(Duration(seconds: 2));
+  //   final Championship campeonatoSerieB = Championship.fromMap(brasileiraoB);
+  //   return [campeonatoSerieB];
+  // }
 
   HomeCubic({required this.getAllChampionshipsUseCase}) : super(HomeInitial());
 
@@ -55,8 +55,8 @@ class HomeCubic extends Cubit<HomeState> {
     try {
       emit(HomeLoading());
       //for test only
-      championschips ??= await getCampeonato();
-      // championschips ??= await getAllChampionshipsUseCase.call();
+      // championschips ??= await getCampeonato();
+      championschips ??= await getAllChampionshipsUseCase.call();
       debugPrint(championschips.toString());
       if (championschips != null && championschips!.isNotEmpty) {
         emit(HomeSuccess(championships: championschips!));
